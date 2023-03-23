0db4a5f3747a490aec15d1bac75604ab9ec9436a
Rudis <82242977+krakatua@users.noreply.github.com>
2023-03-23 19:32:54 -0400

    Landing almost done

---
+++ components/Thumbnail.tsx
@@ -0,0 +1,28 @@
+import { Movie } from "../typings"
+import Image from "next/image"
+
+interface props {
+    //movie: Movie | DocumentData
+    movie: Movie
+}
+
+
+function Thumbnail({movie}: props) {
+    return (
+        <div
+      className={`relative h-28 min-w-[180px] cursor-pointer transition duration-200 ease-out md:h-36 md:min-w-[260px] md:hover:scale-105`}
+      
+    >
+      <Image
+        src={`https://image.tmdb.org/t/p/w500${
+          movie.backdrop_path || movie.poster_path
+        }`}
+        className="rounded-sm object-cover md:rounded"
+        fill
+        alt=""
+      />
+    </div>
+    )
+}
+
+export default Thumbnail
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,9 +3,9 @@ import Header from '../../components/Header'
 import Banner from '../../components/Banner'
 import requests from '../../utils/requests'
 import { Movie } from '../../typings'
+import Row from '../../components/Row'
 
 
-console.log(requests)
 
 interface props {
   netflixOriginals: Movie[] 
@@ -29,7 +29,6 @@ const Home = ({netflixOriginals,
   topRated,
   trendingNow,}: props) => {
 
-    console.log(netflixOriginals)
   
 
 
@@ -46,13 +45,15 @@ const Home = ({netflixOriginals,
         <Header />
         <main className='relative pl-4 pb-24 lg:space-y-24 lg:pl-16'>
           <Banner netflixOriginals={netflixOriginals}/>
-          <section>
-            { }
-            { }
-            { }
-            { }
-            { }
-            { }
+          <section className='md:space-y-24'>
+          <Row title="Trending Now" movies={trendingNow} />
+          <Row title="Top Rated" movies={topRated} />
+          <Row title="Action Thrillers" movies={actionMovies} />
+          {/* My List */}
+          <Row title="Comedies" movies={comedyMovies} />
+          <Row title="Scary Movies" movies={horrorMovies} />
+          <Row title="Romance Movies" movies={romanceMovies} />
+          <Row title="Documentaries" movies={documentaries} />
           </section>
         </main>
         { }

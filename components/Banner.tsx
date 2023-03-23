bf1a0a01f57da50384a794a3589ef0e40ed7b1e4
Rudis <82242977+krakatua@users.noreply.github.com>
2023-03-23 12:46:52 -0400

    first commit

---
+++ components/Banner.tsx
@@ -0,0 +1,27 @@
+import Image from "next/image"
+import { useEffect, useState } from "react"
+import { Movie } from "../typings"
+
+interface Props {
+    netflixOriginals: Movie[]
+}
+
+function Banner ({netflixOriginals}: Props) {
+
+    const [movie, setMovie] = useState<Movie | null>(null)
+
+    useEffect(() => {
+      /* setMovie(netflixOriginals[Math.floor(Math.random() * netflixOriginals.length)]) */
+    }, [netflixOriginals]) 
+
+
+    return (
+        <div>
+            <div>
+                
+            </div>
+        </div>
+    )
+}
+
+export default Banner
